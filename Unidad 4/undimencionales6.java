80e1774a070072c00f9b0ac8a93f75c83a8a40c8
AlexLessus <118391189+AlexLessus@users.noreply.github.com>
2022-11-16 13:51:43 -0600

    Update undimencionales6.java

--- Unidad 4/undimencionales6.java
+++ Unidad 4/undimencionales6.java
@@ -1,3 +1,8 @@
+/*
+   6. Hacer un programa que almacene 10 numeros enteros en un vector. Intercambie los numeros que están en las primeras
+   cinco posiciones por los que están en las ultimas 5 posiciones. Imprima el vector. 
+   Nota: No debe utilizar vector(es) auxiliar(es).
+*/
 import java.util.Scanner;
 public class undimencionales6{
    public static void main(String args[]){
@@ -8,17 +13,14 @@ public class undimencionales6{
          System.out.printf("numero[%d]: ", i);
          num[i] = get.nextInt();
       }
-      
       for(int i = 0; i<5; i++){
          aux = num[i];
          num[i] = num[numF];
          num[numF] = aux;
          numF--;
-      }
-            
+      }          
       for(int i = 0; i<10; i++){
          System.out.printf("\nNum[%d]: %d", i, num[i]);
-         
       }
    }
 }
